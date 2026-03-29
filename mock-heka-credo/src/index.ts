fe036333f16fb8c7e3b320bb00d37b1b4886a660
darshit2308 <darshit2308@gmail.com>
2026-03-29 16:45:17 +0530

    feat: implement GPG challenge-response onboarding flow

--- mock-heka-credo/src/index.ts
+++ mock-heka-credo/src/index.ts
@@ -1,3 +1,7 @@
+import * as openpgp from 'openpgp'
+import axios from 'axios'
+import crypto from 'crypto'
+
 import '@hyperledger/aries-askar-nodejs'
 
 import express from 'express'
@@ -19,6 +23,12 @@ import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
  * We will replace with some persistent DB later
  */
 const identityStore: Record<string, { did: string; vc: unknown }> = {}
+
+/**
+ * In-memory store for pending authentication challenge
+ */
+const challengeStore: Record<string, { nonce: string; expiresAt: number }> = {}
+
 
 const app = express()
 app.use(express.json())
@@ -71,13 +81,103 @@ function setupRoutes(agent: Agent, issuerDid: string) {
     })
   })
 
-  // When the user goes to the heka for the first time for generating DID
-  app.post('/onboard', async (req, res) => {
-    const { github_username } = req.body
+  // This route is used to get the Github GPG key
+  app.get('/challenge/:username', async(req, res) => {
+    const github_username = req.params.username;
 
-    if (!github_username) {
+    try {
+      // We fetch the user's GPG key directly from the github
+      const githubResponse = await axios.get(`https://github.com/${github_username}.gpg`)
+      // The armored key is just a human readable format of the raw keys, which we convert
+      // The raw keys may not be a valid text encoding(UTF-8), so the system may crash
+      // Thats why we use Base64 encoding to convert it.
+      const armoredKeys = githubResponse.data
+
+      if (!armoredKeys || armoredKeys.trim().length === 0) {
+        return res.status(400).json({error: `No GPG keys found for Github user @${github_username}. Please add a GPG key to your Github account `})
+      }
+      // Now, the backend generates a random, highly secure 32-character hex string (nonce)
+      const nonce = crypto.randomBytes(16).toString('hex')
+
+      // Now, we need to store it in server's memory, since it would be used to compare with the user's generate string
+
+      challengeStore[github_username] = {
+        nonce, 
+        expiresAt: Date.now() + 5 * 60 * 1000 // 5 minutes in future
+      }
+
+      //now, w cannot directly use the gpg key, you need to extract it firstly
+      const publicKeys = await openpgp.readKeys({armoredKeys});
+
+      // Now, we convert that random string(nonce) into an encryptable format
+      const message = await openpgp.createMessage({text:nonce})
+      
+      // Now encrypt the nonce and public key
+      const encryptchallenge = await openpgp.encrypt({
+        message, 
+        encryptionKeys: publicKeys,
+      })
+
+      console.log(`Challenge generated for @${github_username}`)
+
+      // Now sent this encrypted message to the user
+      return res.json({
+        message: 'Decrypt this PGP message and send the orignal text to /onboard', 
+        encrypted_challenge: encryptchallenge
+      })
+    } catch(error: any) {
+      if(error.response?.status == 404) {
+        return res.status(404).json({error: `GitHub user @${github_username} does not exist.`})
+      }
+      console.error(`challenge generation error: `, error)
+      return res.status(500).json({error: 'Failed to generate challenge'})
+
+    }
+  })
+
+  // When the user goes to the heka for the first time for generating DID
+  // We will let the user onboard, only if the user provides the correct decrypted message(that randome nonce)
+  app.post('/onboard', async (req, res) => {
+    const { github_username, decrypted_challenge } = req.body // user generates the decrypted message
+
+    if (!github_username || !decrypted_challenge) {
       return res.status(400).json({
-        error: 'github_username is required',
+        error: 'github_username and decrypted_challenge, both required',
+      })
+    }
+
+    // fetching the pending challenge from server's memory
+    const pendingChallenge = challengeStore[github_username]
+
+    if(!pendingChallenge) {
+      return res.status(401).json({
+        error: `No pending challenge found. Please call /challenge/:username first .`,
+      })
+    }
+
+    // Now we check, if the challenge timed out 
+    if(Date.now() > pendingChallenge.expiresAt) {
+      delete challengeStore[github_username]; // remove the expired challenge
+      return res.status(401).json({
+        error: 'Challenge expired, please request a new one !!' ,
+      })
+    }
+
+    // Now we check, if the decrypted message was equal to nonce
+    if(pendingChallenge.nonce != decrypted_challenge){
+      return res.status(401).json({
+        error: 'Invalid challenge response. Decryption failed, or the decrypted message was wrong'
+      })
+    }
+    delete challengeStore[github_username]
+    console.log(`GPG authentication successfull for @${github_username} `)
+
+    if (identityStore[github_username]) {
+      console.log(`ℹ️  @${github_username} is already onboarded. Returning existing credential.`)
+      return res.json({
+        message: 'Already onboarded. Returning existing credential.',
+        did: identityStore[github_username].did,
+        credential: identityStore[github_username].vc,
       })
     }
 
